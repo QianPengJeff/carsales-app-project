1b84f8752bf67859eda232e5aaed44d4544d29a9
QianPengJeff <jimpeng1990@hotmail.com>
2018-03-27 10:24:09 +1100

    created feature-selector component

--- src/app/features-selector/features-selector.component.ts
+++ src/app/features-selector/features-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-features-selector',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./features-selector.component.css']
 })
 export class FeaturesSelectorComponent implements OnInit {
+
+  @Input() public parentData;
 
   constructor() { }
 
