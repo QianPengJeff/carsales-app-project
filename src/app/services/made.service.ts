21769b98f4139760e6185fbec0ba24ec5292dc04
QianPengJeff <jimpeng1990@hotmail.com>
2018-03-26 20:19:40 +1100

    added data to made service and intergrated with the edit-vehicle page

--- src/app/services/made.service.ts
+++ src/app/services/made.service.ts
@@ -5,4 +5,29 @@ export class MadeService {
 
   constructor() { }
 
+  getMadeData(){
+    return [
+        {
+           "id":1,
+           "name":"Audi"
+        },
+        {
+           "id":2,
+           "name":"BMW"
+        },
+        {
+           "id":3,
+           "name":"Ford"
+        },
+        {
+           "id":4,
+           "name":"Holden"
+        },
+        {
+           "id":5,
+           "name":"Hyundai"
+        }
+     ];
+  }
+
 }
