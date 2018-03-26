04c2af3a94f394f8b2ab316f81607ea0fd3c1047
QianPengJeff <jimpeng1990@hotmail.com>
2018-03-26 20:28:51 +1100

    added data to model service and integrated with edit-vehicle page

--- src/app/services/model.service.ts
+++ src/app/services/model.service.ts
@@ -5,4 +5,28 @@ export class ModelService {
 
   constructor() { }
 
+  getModelData() {
+    return [
+        {
+           "id":1,
+           "name":"A1"
+        },
+        {
+           "id":2,
+           "name":"A2"
+        },
+        {
+           "id":3,
+           "name":"A3"
+        },
+        {
+           "id":4,
+           "name":"A4"
+        },
+        {
+           "id":5,
+           "name":"A5"
+        }
+     ];
+  }
 }
